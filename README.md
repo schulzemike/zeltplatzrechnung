3344d7a796d285000128d6b02bbb2b0d02d3d8a1
Mike Schulze <mike.schulze@adesso.de>
2018-09-13 22:12:03 +0200

    added MySQL configuration
    - deactivated Liquibase for the time being to allow Server start

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ A small test project created with Spring Boot and Thymeleaf.
   - MySQL driver
   - Thymemeaf
   - Liquibase
+  
+The application requires a connection to a MySQL database. 
+  
+## Setup the dev environment
+- Clone the repository
+- create a _application-dev.properties_ inside the src/main/resources directory, add the DB configuration parameters from the _application.properties_ and set them according to your development environmen
--- build.gradle.kts
+++ build.gradle.kts
@@ -37,13 +37,13 @@ dependencies {
   compile("org.springframework.boot:spring-boot-starter-security:2.0.5.RELEASE")
   compile("org.springframework.boot:spring-boot-starter-thymeleaf:2.0.5.RELEASE")
   compile("org.springframework.boot:spring-boot-starter-web:2.0.5.RELEASE")
-  compile("org.liquibase:liquibase-core:3.6.2")
+//  compile("org.liquibase:liquibase-core:3.6.2")
   compile("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:2.3.0")
   compile("org.thymeleaf.extras:thymeleaf-extras-java8time:3.0.1.RELEASE")
 
   compileOnly("org.projectlombok:lombok:1.18.2")
 
-  runtime("mysql:mysql-connector-java:8.0.11")
+  compile("mysql:mysql-connector-java:8.0.11")
   runtime("org.springframework.boot:spring-boot-devtools:2.0.3.RELEASE")
 
   testCompile("org.springframework.boot:spring-boot-starter-test:2.0.3.RELEASE")
